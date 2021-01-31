fc762816881601dae315728a5c9d15de66723f45
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-01-31 17:30:26 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
 
 export default class Comment extends Component {
   render() {
     return (
       <div className="comment">
-      {this.props.commentText}
+        {this.props.commentText}
       </div>
-    );
+    )
   }
 }
-
